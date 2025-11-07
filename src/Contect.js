dbc1aa0b421046fff78d011a64e6394365267680
Arulselvam-S <senthilkumarar8@gmail.com>
2025-11-07 15:12:07 +0530

    fivith

--- src/Contect.js
+++ src/Contect.js
@@ -8,6 +8,7 @@ import EmailIcon from '@mui/icons-material/Email';
 import arul  from './assets/arul.jpg'
 import logo from './assets/logo.webp'
 import { useState } from 'react'
+import axios from 'axios';  
 
 const Contect = () => {
    const [pic,setpic] = useState(arul)
@@ -24,7 +25,19 @@ const Contect = () => {
     function back2(){
       setpi(arul)
     }
-    
+    const[name,setName]=useState('');
+    const[email,setEmail]=useState('');
+    const[phone,setPhone]=useState('');
+    const[address,setAddress]=useState('');
+    function postevent(){
+      axios.post("http://localhost:5000/todo/contactpost",{
+        name, email, phone, address
+      }).then((res)=>{
+        alert("Contact saved successfully 😁")
+      }).catch((err)=>{
+        console.error(err);
+      })
+    }
   return (
     <div className="Contect"> 
      <div className="image">
@@ -38,11 +51,32 @@ const Contect = () => {
       autoComplete="off"
       className="Form"
     >
-      <TextField id="outlined-basic" label="Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Email" variant="outlined" />
-      <TextField id="outlined-basic" label="Phone" variant="outlined" />
-      <TextField id="outlined-basic" label="Adderess" variant="outlined" />
-      <button className="btn-submit">Submit</button>
+      <TextField id="outlined-basic" 
+      label="Name"
+      variant="outlined"
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+       />
+      <TextField id="outlined-basic" 
+      label="Email" 
+      variant="outlined"
+      value={email}
+      onChange={(e)=>setEmail(e.target.value)}
+      />
+      <TextField id="outlined-basic"
+       label="Phone" 
+       variant="outlined" 
+        value={phone}
+        onChange={(e)=>setPhone(e.target.value)}
+       />
+      <TextField id="outlined-basic" label="Adderess" 
+      variant="outlined"
+      value={address}
+      onChange={(e)=>setAddress(e.target.value)}
+       />
+      <button className="btn-submit"
+      onClick={postevent}
+      >Submit</button>
         <div className="icon">
             <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile" className="a1"><LinkedInIcon/></a>
            <a href="https://www.instagram.com/_mr__parker__/" className="a2"> <InstagramIcon/></a>
